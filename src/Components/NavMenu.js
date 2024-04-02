ca50239741bc5528f423220c8f874ad74de70b70
cyweyo <cyw7715@gmail.com>
2024-04-02 23:12:15 +0900

    welcome 화면 추가

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { useLocation } from 'react-router-dom';
 
@@ -7,11 +8,17 @@ export default function NavMenu({ activeKey, setActiveKey, navigate }) {
 
   useEffect(() => {
     switch (location.pathname) {
-      case '/color':
+      case '/calculator':
         setActiveKey(2);
         break;
-      case '/memo':
+      case '/color':
         setActiveKey(3);
+        break;
+      case '/memo':
+        setActiveKey(4);
+        break;
+      case '/test':
+        setActiveKey(5);
         break;
       default:
         setActiveKey(1);
@@ -20,24 +27,41 @@ export default function NavMenu({ activeKey, setActiveKey, navigate }) {
   }, [location.pathname, setActiveKey]);
 
   return (
-    <nav className="flex justify-center">
-      <Nav variant="pills" activeKey={activeKey} onSelect={(key) => setActiveKey(key)}>
-        <Nav.Item>
-          <Nav.Link eventKey={1} to='/' onClick={() => navigate('/')}>
-            Calculator
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey={2} to='/color' onClick={() => navigate('/color')}>
-            Color Selector
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey={3} to='memo' onClick={() => navigate('/memo')}>
-            Memo
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </nav>
+    <>
+      <h1 className="text-3xl font-bold flex justify-center my-8">
+        Convenient Tool Set
+      </h1>
+      <nav className="flex justify-center">
+        <Container className='flex justify-center'>
+          <Nav variant="pills" activeKey={activeKey} onSelect={(key) => setActiveKey(key)}>
+            <Nav.Item>
+              <Nav.Link eventKey={1} to='/' onClick={() => navigate('/')}>
+                Welcome
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={2} to='/' onClick={() => navigate('/calculator')}>
+                Calculator
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={3} to='/color' onClick={() => navigate('/color')}>
+                Color Selector
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={4} to='memo' onClick={() => navigate('/memo')}>
+                Memo
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={5} to='test' onClick={() => navigate('/test')}>
+                Test
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </nav>
+    </>
   );
 }
--- src/Components/ToolSet.js
+++ src/Components/ToolSet.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Calculator from "./Calculator";
 import Memo from "./Memo";
 import ColorSelector from "./ColorSelector";
+import Welcome from "./Welcome";
 
-export default function ToolSet() {
+export default function ToolSet({ latitude, longitude, accuracy }) {
   return (
     <div className="flex justify-center mt-20">
       <Routes>
         <Route path="*" element={<div>없는페이지임</div>} />
-        <Route path='/' element={<Calculator />} />
+        <Route path='/' element={<Welcome latitude={latitude} longitude={longitude} accuracy={accuracy} />} />
+        <Route path='/calculator' element={<Calculator />} />
         <Route path='/color' element={<ColorSelector />} />
         <Route path='/memo' element={<Memo />} />
       </Routes>
