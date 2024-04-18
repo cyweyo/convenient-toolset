b909a607f21c3145a45b5319866056044362c792
cyweyo <cyw7715@gmail.com>
2024-04-19 05:57:17 +0900

    라우터 수정

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { useLocation } from 'react-router-dom';
 
@@ -9,59 +9,52 @@ export default function NavMenu({ activeKey, setActiveKey, navigate }) {
   useEffect(() => {
     switch (location.pathname) {
       case '/calculator':
-        setActiveKey(2);
+        setActiveKey(1);
         break;
       case '/color':
+        setActiveKey(2);
+        break;
+      case '/todolist':
         setActiveKey(3);
         break;
-      case '/memo':
+      case '/test':
         setActiveKey(4);
         break;
-      case '/test':
-        setActiveKey(5);
-        break;
       default:
-        setActiveKey(1);
+        setActiveKey("");
         break;
     }
   }, [location.pathname, setActiveKey]);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold flex justify-center my-8">
-        Convenient Tool Set
-      </h1>
-      <nav className="flex justify-center">
-        <Container className='flex justify-center'>
-          <Nav variant="pills" activeKey={activeKey} onSelect={(key) => setActiveKey(key)}>
-            <Nav.Item>
-              <Nav.Link eventKey={1} to='/' onClick={() => navigate('/')}>
-                Welcome
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey={2} to='/' onClick={() => navigate('/calculator')}>
-                Calculator
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey={3} to='/color' onClick={() => navigate('/color')}>
-                Color Selector
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey={4} to='memo' onClick={() => navigate('/memo')}>
-                Memo
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey={5} to='test' onClick={() => navigate('/test')}>
-                Test
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </nav>
-    </>
+    <Navbar bg="dark" variant="dark" className='flex'>
+      <Container className='flex'>
+        <Nav variant="pills" activeKey={activeKey} onSelect={(key) => setActiveKey(key)}>
+          <Navbar.Brand href='/' className='flex justify-center'>
+            Convenient Tool Set
+          </Navbar.Brand>
+          <Nav.Item>
+            <Nav.Link eventKey={1} to='/calculator' onClick={() => navigate('/calculator')}>
+              Calculator
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={2} to='/color' onClick={() => navigate('/color')}>
+              Color Selector
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={3} to='todolist' onClick={() => navigate('/todolist')}>
+              Todolist
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={4} to='test' onClick={() => navigate('/test')}>
+              Test
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
