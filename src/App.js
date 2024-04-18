24b7483558556aeb550e154f05610a0e1494a928
cyweyo <cyw7715@gmail.com>
2024-04-19 05:56:51 +0900

    useEffect 훅 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavMenu from './Components/NavMenu'
 import ToolSet from './Components/ToolSet';
 import { useState } from 'react';
@@ -6,14 +6,16 @@ import { useNavigate } from 'react-router-dom';
 
 
 function App() {
-  const [activeKey, setActiveKey] = useState(1);
   const navigate = useNavigate();
+  const [activeKey, setActiveKey] = useState(1);
+
+
+  useEffect(() => {
+
+  }, [activeKey])
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline flex justify-center my-8">
-        Convenient Tool Set
-      </h1>
       <NavMenu activeKey={activeKey} setActiveKey={setActiveKey} navigate={navigate} />
       <ToolSet />
     </>
