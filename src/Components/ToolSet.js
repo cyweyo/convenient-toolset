64507161ed7b10086fa6977e8250658815048947
cyweyo <cyw7715@gmail.com>
2024-04-19 05:57:52 +0900

    라우터 수정함

--- src/Components/ToolSet.js
+++ src/Components/ToolSet.js
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Calculator from "./Calculator";
-import Memo from "./Memo";
+import TodoList from "./TodoList";
 import ColorSelector from "./ColorSelector";
-import Welcome from "./Welcome";
+import Main from "./Main";
+import Test from "./Test";
 
 export default function ToolSet({ latitude, longitude, accuracy }) {
   return (
     <div className="flex justify-center mt-20">
       <Routes>
-        <Route path="*" element={<div>없는페이지임</div>} />
-        <Route path='/' element={<Welcome latitude={latitude} longitude={longitude} accuracy={accuracy} />} />
+        <Route path='/' element={<Main latitude={latitude} longitude={longitude} accuracy={accuracy} />} />
         <Route path='/calculator' element={<Calculator />} />
         <Route path='/color' element={<ColorSelector />} />
-        <Route path='/memo' element={<Memo />} />
+        <Route path='/TodoList' element={<TodoList />} />
+        <Route path='/test' element={<Test />} />
+        <Route path="*" element={<div>없는페이지임</div>} />
       </Routes>
     </div>
   )
