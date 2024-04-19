daa95ba714ad870fcbf5e5de346880ed00837194
cyweyo <cyw7715@gmail.com>
2024-04-20 01:31:59 +0900

    구글 맵 추가

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
 export default function Main() {
+  const containerStyle = {
+    width: '550px',
+    height: '550px'
+  };
+
+
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -20,12 +27,30 @@ export default function Main() {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 
+  const center = {
+    lat: 37.51939392089844,
+    lng: 126.88313293457031
+  };
+
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-
   return (
-    <>
-      <div>main</div>
-    </>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-lg mb-10'>오늘의 날씨</h1>
+      <LoadScript
+        googleMapsApiKey="AIzaSyD_LX9JZ9DE59NUzg4ufILbmGSm-FfoXVo"
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={14}
+        >
+          <Marker
+            position={center}
+            title="My location"
+          />
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 }
